103f301e8d08a0ad46dd43462983e475f2a203f4
Yugam Pahwa <59441037+yugampahwa@users.noreply.github.com>
2021-01-16 20:39:51 +0530

    Add files via upload

--- Stack/Stack.py
+++ Stack/Stack.py
@@ -1,1 +1,28 @@
+class Stack:
+    stack = []
 
+    def size(self):
+        return len(self.stack)
+
+    def isEmpty(self):
+        return self.size() == 0
+
+    def push(self, elem):
+        self.stack.append(elem)
+
+    def pop(self):
+        if self.isEmpty():
+            raise RuntimeError('Empty Stack!!!')
+        data = self.stack[len(self.stack) - 1]
+        self.stack = self.stack[:len(self.stack) - 1]
+        print("Dropped :", data)
+
+    def peek(self):
+        if self.isEmpty():
+            raise RuntimeError("Empty Stack!!!")
+        data = self.stack[len(self.stack) - 1]
+        print("peek element:",data)
+
+    def printStack(self):
+        for x in self.stack:
+            print(x)
